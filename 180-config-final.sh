05a7f09d48c01ef2eeeab36ab83c6731e7f3eeee
tfreese <commercial@freese-home.de>
2019-09-25 15:49:16 +0200

    sysctl params

--- 180-config-final.sh
+++ 180-config-final.sh
@@ -23,10 +23,19 @@ sed -i 's/tcp6/#tcp6/' /etc/netconfig;
 
 # Config sysctl
 cat << EOF > /etc/sysctl.d/10-sysctl.conf
+
+# Möglichst wenig Swappen, 0 (aus) - 100 (max)
 vm.swappiness=10
+# cat /proc/sys/vm/swappiness
 
 dev.raid.speed_limit_min = 50000
 dev.raid.speed_limit_max = 200000
+
+# TCP SYN Flood Protection
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_max_syn_backlog = 2048
+net.ipv4.tcp_synack_retries = 3
+# cat /proc/sys/net/ipv4/tcp_...
 
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
@@ -34,6 +43,9 @@ net.ipv6.conf.lo.disable_ipv6 = 1
 net.ipv6.conf.eth0.disable_ipv6 = 1
 net.ipv6.conf.wlan0.disable_ipv6 = 1
 EOF
+
+# Reload
+systemctl restart systemd-sysctl.service;
 
 # Festplaten-Caching deaktivieren (nur bei Raid)
 pacman --noconfirm --needed -S hdparm;
