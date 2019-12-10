422cff5403b5f57841ee58192296d484b691825f
tfreese <commercial@freese-home.de>
2019-12-10 17:25:03 +0100

    polish

--- 110-create-user.sh
+++ 110-create-user.sh
@@ -38,4 +38,33 @@ EOF
 chown -R "$USER_NAME":"$GROUP_NAME" "/home/$USER_NAME";
 
 # wheel UserGroup für sudo berechtigen
-sed -i_"$TIME" 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers;
+#sed -i_"$TIME" 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers;
+
+echo "## (the '#' here does not indicate a comment)" >> /etc/sudoers;
+echo "#includedir /etc/sudoers.d" >> /etc/sudoers;
+
+cat << EOF > /etc/sudoers.d/tommy
+# Reset environment by default
+Defaults      env_reset
+
+# Set default EDITOR to nano, and do not allow visudo to use EDITOR/VISUAL.
+Defaults      editor=/usr/bin/nano, !env_editor
+
+
+## Uncomment to allow members of group wheel to execute any command
+%wheel ALL=(ALL) ALL
+
+
+#User_Alias   ADMINS = USER1, USER2, USER3
+#ADMINS ALL=(ALL) NOPASSWD: ALL
+
+
+#Cmnd_Alias      SHUTDOWN = /usr/bin/shutdown, /usr/bin/systemctl poweroff, /usr/bin/systemctl stop kodi.service
+Cmnd_Alias      MAINTAIN = /usr/bin/lsblk, /usr/bin/hddtemp, /usr/bin/smartctl, /usr/bin/nvme
+Cmnd_Alias      LVM = /usr/bin/pvs, /usr/bin/vgs, /usr/bin/lvs
+
+tommy ALL=(ALL) NOPASSWD: MAINTAIN, LVM
+EOF
+
+chmod 0440 /etc/sudoers.d/tommy;
+visudo -c [/etc/sudoers.d/tommy];
